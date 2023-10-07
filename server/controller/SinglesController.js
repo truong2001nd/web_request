00ce9e1db6eb4fc74e47325349d5c6022f784ae4
Truong2001nd <phamtruong14102001@gmail.com>
2023-10-07 21:48:54 +0700

    don

--- server/controller/SinglesController.js
+++ server/controller/SinglesController.js
@@ -1,10 +1,16 @@
+const { request } = require("express");
 const Singles = require("../models/Singles.js");
 const SingleType = require("../models/SingleTypes.js");
 const User = require("../models/Users.js");
 
 const createSingle = async (req, res, next) => {
-  const { name, content, singlesStyes, sender, approver } = req.body;
-  if (!name || !content || !singlesStyes || !sender || !approver) {
+  if (!req.permissions.single.includes("create")) {
+    return res
+      .status(401)
+      .json({ success: false, message: "Tài khoản không có quyền truy cập" });
+  }
+  const { name, content, singlesStyes, approver } = req.body;
+  if (!name || !content || !singlesStyes || !approver) {
     return res
       .status(400)
       .json({ success: false, message: "Mời nhập đủ dữ liệu đầu vào" });
@@ -24,13 +30,6 @@ const createSingle = async (req, res, next) => {
         .status(401)
         .json({ success: false, message: "Không tồn tại Loại đơn" });
     }
-    const senderRelease = await User.findOne({ _id: sender });
-
-    if (!senderRelease) {
-      return res
-        .status(401)
-        .json({ success: false, message: "Mã người gửi không tồn tại" });
-    }
     const approverRelease = await User.findOne({ _id: approver });
 
     if (!approverRelease) {
@@ -38,17 +37,12 @@ const createSingle = async (req, res, next) => {
         .status(401)
         .json({ success: false, message: "Mã người phê duyệt không tồn tại" });
     }
-    if (senderRelease === approverRelease) {
-      return res.status(401).json({
-        success: false,
-        message: "Bạn không thể gửi đơn đến bạn được",
-      });
-    }
+
     const newSingles = new Singles({
       name,
       content: JSON.stringify(content),
       singlesStyes,
-      sender,
+      sender: req.userId,
       approver,
       status: 0,
     });
@@ -67,4 +61,31 @@ const createSingle = async (req, res, next) => {
   }
 };
 
-module.exports = { createSingle };
+const getAllSingle = async (req, res) => {
+  // check quyen
+
+  if (!req.permissions.single.includes("read")) {
+    return res
+      .status(401)
+      .json({ success: false, message: "Tài khoản không có quyền truy cập" });
+  }
+
+  // check quyen
+
+  try {
+    const singleAll = await Singles.find();
+
+    if (!singleAll) {
+      return res
+        .status(401)
+        .json({ success: false, message: "Chưa có đơn nào được tạo!" });
+    }
+
+    res.json({ success: true, data: singleAll });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ success: false, message: "Dịch vụ bị gián đoạn" });
+  }
+};
+
+module.exports = { createSingle, getAllSingle };
--- server/routes/singlesRouter.js
+++ server/routes/singlesRouter.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const verifyToken = require("../middleware/verifyToken");
 
-const { createSingle } = require("../controller/SinglesController.js");
+const {
+  createSingle,
+  getAllSingle,
+} = require("../controller/SinglesController.js");
 
 const router = express.Router();
 
-router.post("/create", createSingle);
-// router.get("/getOne/:id", verifyToken, getOneRoom);
-// router.get("/getAll", verifyToken, getAllRoom);
+router.post("/create", verifyToken, createSingle);
+router.get("/getAll", verifyToken, getAllSingle);
 // router.put("/update/:id", verifyToken, updateRoom);
 // router.delete("/deleteRoom/:id", verifyToken, deleteRoom);
 
